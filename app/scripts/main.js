f1544abf23b76c67dd3cfceaa5e1b34578388b2a
slkennedy <slkennedy@gmail.com>
2014-10-04 08:50:30 -0400

    Adds layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('The Iron Yard Rocks');
+/*global Backbone, _, $ */
+(function (){
+	'use strict',
+
+//Models
+var Foods = Backbone.Model.extend ({
+	defaults: {
+		name: "",
+		price: 10,
+		description: "..."
+	}
+});
+
+//Collections
+
+//Views
+	var MenuView = Backbone.View.extend ({
+		tagName: "ul",
+		className: "menu-list",
+
+		initialize: function (options){
+			options = options || {};
+			this.$container = options.$container;
+			this.$container.append(this.el);
+		},
+
+		render: function(){
+
+		}
+	}); 
+
+
+//Glue Code
+	$(document).ready(function(){
+		var food = [
+		{name: "name", price: 12, description: "This is some food and here is the description", type: "app"}
+		{name: "otherName", price: 20, description: "This is some other food and here is the other food's description", type: "entree"}
+		];
+
+		var eats = new MenuView;
+
+
+
+	});
+})();

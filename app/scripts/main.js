458fa1c90af5a8db93aa63803a6a7088295b4f49
slkennedy <slkennedy@gmail.com>
2014-10-04 19:27:19 -0400

    Adds some styles, Further Attempts to Backbone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,31 +2,84 @@
 (function (){
 	'use strict';
 
-//Models
+	window.App = {};
 
+	App.Views = {};
+	App.Models = {};
+	App.Collections = {};
 
-//Collections
+/////////////////////////////MODELS//////////////
+////////////////////////////////////////////////
 
-//Views
-	var MenuView = Backbone.View.extend ({
+	App.Models.MenuItem = Backbone.Model.extend ({
+		defaults: {
+			name: "",
+			description: "...",
+			price: 0,
+			image: "http://www.302aw.afrc.af.mil/shared/media/document/AFD-090108-058.jpg",
+			type: "appetizer"
+		},
+		firebase: new Backbone.Firebase("https://greenville-thai.firebaseio.com/")
+	});
+
+/////////////////////////////COLLECTIONS/////////
+////////////////////////////////////////////////
+
+	App.Collections.AllItems = Backbone.Collection.extend ({
+		model: App.Models.MenuItem,
+		firebase: "https://greenville-thai.firebaseio.com/"
+	});
+
+/////////////////////////////VIEWS///////////////
+////////////////////////////////////////////////
+
+	//Items View to render all of the items from the JSON file on Firebase
+	App.Views.ItemsView = Backbone.View.extend ({
+		tagName: 'li',
+		className: 'menu-item',
+		template: _.template($("#templates-menu-item").text()),
+
+		initialize: function (){
+			console.log("this is: "+this);
+			this.collection.each(function(item){
+				console.log("this is each of the collection"+item);
+			})
+		},
+
+		render: function (){
+			this.$el.html(this.template(this.model));
+			$('.menu-list').append(this.el);
+		},
+	});
+
+	//Menu View to hold all of the menu items from ItemsView
+	App.Views.MenuView = Backbone.View.extend ({
 		tagName: "ul",
 		className: "menu-list",
 
+		initialize: function (){
+			this.render();
+			// var items = new App.Views.ItemsView;
+			// items.render();
+		},
+
 		render: function(){
 			$('.content-wrapper').append(this.el);
-			console.log(this);
 		}
 	}); 
 
-	
-
-//Glue Code
+/////////////////////////////GLUE CODE///////////
+////////////////////////////////////////////////
 	$(document).ready(function(){
-		var menuView = new MenuView();
-		menuView.render();
+		var allItems = new App.Collections.AllItems();
 
+		new App.Views.ItemsView ({
+			collection: allItems
+		});
 
-
+		new App.Views.MenuView({
+			collection: allItems
+		});
 
 	});
 })();
